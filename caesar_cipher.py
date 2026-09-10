926954fea67e21bbc3f469d3346771b4e461e3e4
Joe Mock <joemock1011@gmail.com>
2026-09-10 13:23:29 -0700

    Adding bare bones implementation for encryption function

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,7 +2,7 @@ import random
 
 # Key will be an integer randomized
 # Key represents the number of letters to shift in the alphabet (our 27 character alphabet)
-KEY = 0
+KEY = 1
 
 
 def enc(message: str) -> str:
@@ -13,6 +13,7 @@ def enc(message: str) -> str:
     '''
     for letter in message:
         letter = letter + KEY
+
     pass
 
 def dec(message_enc: str) -> str:
@@ -34,11 +35,38 @@ def gen():
     
 
 
-message = "Hello World"
+message = "MESSAGZ"
+#print(message)
+
+#message_enc = enc(message)
+#print(message_enc)
+
+#message_dec = dec(message_enc)
+#print(message_dec)
+
+# This will be the encryption function
+enc_message = ""
+for i, letter in enumerate(message):
+    if letter.isalpha():
+        num = ord(letter)
+        if num + KEY > 90:
+            #print("Rollover handling")
+            #print(num)
+            new_char_ascii = (num + KEY - 90) + 63
+            #print(new_char_ascii)
+        elif num == 20:
+            new_char_ascii = 63 + KEY
+        else:
+            new_char_ascii = num + KEY
+
+        if new_char_ascii == 64:
+            new_char_ascii = 32
+        enc_letter = chr(new_char_ascii)
+        enc_message = enc_message + enc_letter
+
+
+
 print(message)
+print(enc_message)
 
-message_enc = enc(message)
-print(message_enc)
-
-message_dec = dec(message_enc)
-print(message_dec)
+# Now we can do the decryption function
